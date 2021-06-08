bf6e3e2d4ca2fc52277d540e26296f1d2868dc99
VladBeglik <vladbeglik@gmail.com>
2021-06-08 14:10:08 +0300

    firsrt task

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int max = 0;
+			int count = 0;
+			string a = Console.ReadLine();
+
+			for(int i = 0; i < a.Length - 1; i++)
+			{
+				if(a[i] != a[i + 1])
+				{
+					count++;
+				}
+				else
+				{
+					count = 0;
+				}
+			}
+
+			if(count > max)
+			{
+				max = count;
+			}
+
+			Console.WriteLine(max+1);
+		}
+	}
+}
