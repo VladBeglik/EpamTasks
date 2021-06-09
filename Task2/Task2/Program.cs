c7984e7717cd48e63bc4cf2b8c81eea3e47bf4a4
VladBeglik <vladbeglik@gmail.com>
2021-06-09 14:07:16 +0300

    task2

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = Convert.ToInt32(Console.ReadLine());
+			int b = Convert.ToInt32(Console.ReadLine());
+			string result = string.Empty;
+			while(a != 0)
+			{
+				result += a % b;
+				a /= b;
+			}
+			char[] arr = result.ToCharArray();
+			Array.Reverse(arr);
+			Console.WriteLine(new string(arr));
+
+
+		}
+	}
+}
